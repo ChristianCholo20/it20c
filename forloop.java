ff3dcfdb6bce840b89d78362959345e54f43c62d
ChristianCholo20 <chologodlike55@gmail.com>
2024-09-20 23:07:07 +0800

    Staged changes: added array examples, modified llenas.java, and tracked new files

---
+++ forloop.java
@@ -0,0 +1,11 @@
+public class ArrayForLoop {
+    public static void main(String[] args) {
+        // Declare an array of integers
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Display array via for loop
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
